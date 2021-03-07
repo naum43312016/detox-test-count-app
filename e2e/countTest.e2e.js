a59ad794513e110ff30686e7fca0c86159c6cbc0
nauma-ottopia <nauma@ottopia.tech>
2021-03-07 21:36:49 +0200

    first commit

---
+++ e2e/countTest.e2e.js
@@ -0,0 +1,49 @@
+describe('Example', () => {
+    beforeAll(async () => {
+      await device.launchApp();
+    });
+  
+    beforeEach(async () => {
+      await device.reloadReactNative();
+    });
+  
+    it('Button Increase should be visible', async () => {
+        await expect(element(by.id('increaseBtn'))).toBeVisible();
+    });
+
+    it('Button Decrease should be visible', async () => {
+        await expect(element(by.id('decreaseBtn'))).toBeVisible();
+    });
+
+    it('Count text should be 0', async () => {
+        await expect(element(by.id('countText'))).toHaveText("0")
+    });
+  
+    it('Increase button functionality', async () => {
+        await expect(element(by.id('countText'))).toHaveText("0")
+        await element(by.id('increaseBtn')).tap();
+        await expect(element(by.id('countText'))).toHaveText("1")
+        await element(by.id('increaseBtn')).tap();
+        await expect(element(by.id('countText'))).toHaveText("2")
+        await element(by.id('increaseBtn')).tap();
+        await expect(element(by.id('countText'))).toHaveText("3")
+    });
+
+    it("Decrease button when count text is 0", async () => {
+        await expect(element(by.id('countText'))).toHaveText("0")
+        await element(by.id('decreaseBtn')).tap();
+        await expect(element(by.id('countText'))).toHaveText("0")
+    })
+
+    it('Increase and Decrease buttons combain functionality', async () => {
+        await expect(element(by.id('countText'))).toHaveText("0")
+        await element(by.id('increaseBtn')).tap();
+        await expect(element(by.id('countText'))).toHaveText("1")
+        await element(by.id('increaseBtn')).tap();
+        await expect(element(by.id('countText'))).toHaveText("2")
+        await element(by.id('decreaseBtn')).tap();
+        await expect(element(by.id('countText'))).toHaveText("1")
+        await element(by.id('increaseBtn')).tap();
+        await expect(element(by.id('countText'))).toHaveText("2")
+    });
+  });
